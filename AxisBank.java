abddabf7e2fb9955c76aade2552d71905f3236ff
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-22 00:38:52 +0530

    AxisBank

---
+++ AxisBank.java
@@ -0,0 +1,15 @@
+package Bank;
+
+public class AxisBank{
+
+	public void Deposit() {
+		// TODO Auto-generated method stub
+		System.out.println("Total Deposit : Overridden Amount 562000000");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
