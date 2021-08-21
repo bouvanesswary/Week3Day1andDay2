6b473fbfb89f9708396131f0a50c36d3ecce3de3
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-21 23:06:47 +0530

    College

---
+++ Collage.java
@@ -0,0 +1,26 @@
+package org.collage;
+
+import org.department.Department;
+
+public class Collage extends Department {
+
+	public void CollegeName() {
+		// TODO Auto-generated method stub
+System.out.println("College Name SMVEC ");
+	}
+	
+	public void CollegeCode() {
+		// TODO Auto-generated method stub
+System.out.println("College Code is A234sf ");
+	}
+	
+	public void CollegeRank() {
+		// TODO Auto-generated method stub
+System.out.println("College Rank is 1");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
