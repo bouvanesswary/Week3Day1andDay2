b7d7446c106cbcc4cc678d23c7b07cbfe84b52e5
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-21 23:14:13 +0530

    Desktop

---
+++ Desktop.java
@@ -0,0 +1,20 @@
+package org.system;
+
+public class Desktop{
+
+	public void DesktopSize() {
+		// TODO Auto-generated method stub
+		System.out.println("DesktopSize");
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Computer com = new Computer();
+		Desktop d=new Desktop();
+		com.ComputerModel();
+		d.DesktopSize();
+		
+	}
+
+}
