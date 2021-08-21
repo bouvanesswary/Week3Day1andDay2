57afb77c64f4e72dc9385b0c67986844b5a311e0
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-21 23:13:58 +0530

    Computer

---
+++ Computer.java
@@ -0,0 +1,16 @@
+package org.system;
+
+public class Computer{
+	
+	public void ComputerModel() {
+		// TODO Auto-generated method stub
+		System.out.println("Computer Model");
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
