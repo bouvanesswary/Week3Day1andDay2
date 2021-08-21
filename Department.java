beab433b46419421541d5d70a202f5da3ad76d43
bouvanesswary <88605685+bouvanesswary@users.noreply.github.com>
2021-08-21 23:07:09 +0530

    Department

---
+++ Department.java
@@ -0,0 +1,17 @@
+package org.department;
+
+
+
+public class Department {
+
+	public void DeptName() {
+		// TODO Auto-generated method stub
+		System.out.println("Department Name : Computer Science ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
